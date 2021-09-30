2373a3210808f62f891ed974b396f43d5daf39ba
MONIL PATEL <90346837+monilpatel1999@users.noreply.github.com>
2021-09-29 23:58:26 -0400

    1st commit

---
+++ src/main/java/ex01/App.java
@@ -0,0 +1,24 @@
+/* Exercise 01
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Monil_Patel
+ */
+package oop.example;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Ayush Pindoria
+ */
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String name;
+        System.out.print("What is your name? ");
+        name = input.nextLine();
+
+        String outname = new String(name);
+        System.out.printf("Hello, %s, nice to meet you!", outname);
+
+    }
+}
